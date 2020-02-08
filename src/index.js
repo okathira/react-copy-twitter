b8cdfd9b71fca88d8d2c86c00b5f4bc0c2228768
okathira <43328895+okathira@users.noreply.github.com>
2020-02-08 19:26:03 +0900

    ツイートの一部を構成

--- src/index.js
+++ src/index.js
@@ -3,11 +3,52 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 
+function TweetText(props) {
+  return (
+    <span className="tweet-text">
+      {props.text}
+    </span>
+  )
+}
+
+function TweetIcon(props) {
+  return (
+    <img className="tweet-icon" src={props.iconURL} alt="icon"></img>
+  )
+}
+
+function TweetDetail(props) {
+  return (
+    <p>
+      <span className="tweet-name">{props.name}</span>
+      <span className="tweet-id">@{props.id}</span>
+    </p>
+  )
+}
+
+class Tweet extends React.Component {
+  render() {
+    const data = {
+      text: "Hello, Twitter.",
+      icon: "https://abs.twimg.com/sticky/default_profile_images/default_profile_bigger.png",
+      name: "お名前",
+      id: "twitterID"
+    }
+    return (
+      <div className="tweet">
+        <TweetIcon iconURL={data.icon} />
+        <TweetDetail name={data.name} id={data.id} />
+        <TweetText text={data.text} />
+      </div>
+    );
+  }
+}
+
 class App extends React.Component {
   render() {
     return (
-      <p>Twitter</p>
-    )
+      <Tweet />
+    );
   }
 }
 

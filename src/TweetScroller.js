baea449d7ef2f3cd136c00743e968bfa70041eae
okathira <43328895+okathira@users.noreply.github.com>
2020-04-23 07:12:09 +0900

    APIのファイル分け

--- src/TweetScroller.js
+++ src/TweetScroller.js
@@ -3,59 +3,7 @@ import DoTweetBox from './DoTweetBox';
 import Tweet from './Tweet';
 import dummyData from './dummyTweetData';
 import { Container, Text } from './uiElements';
-import axios from 'axios';
-
-
-const rfc1738Encode = (str) => {
-  return encodeURIComponent(str)
-    .replace(/!/g, '%21')
-    .replace(/'/g, '%27')
-    .replace(/\(/g, '%28')
-    .replace(/\)/g, '%29')
-    .replace(/\*/g, '%2A');
-};
-
-const twitterAPI = axios.create({
-  baseURL: 'https://api.twitter.com/',
-});
-
-const getBearerToken = async () => {
-  const bearerTokenResponse = await twitterAPI.post(
-    'oauth2/token',
-    'grant_type=client_credentials',
-    {
-      auth: {
-        username: rfc1738Encode(process.env.REACT_APP_TWITTER_API_KEY),
-        password: rfc1738Encode(process.env.REACT_APP_TWITTER_API_SECRET_KEY)
-      },
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
-      },
-    }
-  );
-
-  return bearerTokenResponse.data.access_token;
-};
-
-const getUserTimeline = async (givenToken, screenName) => {
-  const bearerToken = givenToken ? givenToken : await getBearerToken();
-  const userTimelineResponse = await twitterAPI.get(
-    '1.1/statuses/user_timeline.json',
-    {
-      headers: { Authorization: `Bearer ${bearerToken}` },
-      params: {
-        screen_name: screenName,
-        count: 10,
-        exclude_replies: true,
-        include_rts: false,
-        tweet_mode: 'extended',
-      },
-    }
-  );
-  console.log(userTimelineResponse);
-
-  return userTimelineResponse.data;
-};
+import twitterAPI from './twitterAPI';
 
 
 export default class TweetScroller extends React.Component {
@@ -70,7 +18,7 @@ export default class TweetScroller extends React.Component {
   }
 
   componentDidMount() {
-    getUserTimeline(this.state.bearerToken, 'reactjs')
+    twitterAPI.getUserTimeline(this.state.bearerToken, 'reactjs')
       .then(
         timelineTweets => {
           this.setState({
